05111c74e13e92fe6052e46e9829612d6c11b811
iamseohyuniamseohyun <146047867+iamseohyuniamseohyun@users.noreply.github.com>
2023-12-04 12:17:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
         font-family: 'NanumSquareNeo-Variable';
         }
         body {
-        background: url('http://localhost:3002/image.png') no-repeat center center fixed;
+        background: url('https://port-0-memomong-1gksli2alpq681us.sel4.cloudtype.app:3002/image.png') no-repeat center center fixed;
         background-size: cover;
         }
 
